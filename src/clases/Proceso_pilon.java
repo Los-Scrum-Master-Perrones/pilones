315e2923c50ed1b50b9c94c484625336e5f3c56c
KeniaColindress <colindreskenia13@gmail.com>
2021-02-09 14:58:09 -0600

    agregar variables de referencia

--- src/clases/Proceso_pilon.java
+++ src/clases/Proceso_pilon.java
@@ -3,11 +3,34 @@ package clases;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Proceso_pilon extends Aplicacion_principal{
+    public Label lbl_id_procesopilon;
+    public Label lbl_id_proceso_pilon;
+    public Label lbl_fecha_proceso;
+    public Label lbl_id_remi;
+    public Label lbl_entradas_salidas;
+    public Label lbl_id_tab;
+    public Label lbl_id_pil;
+    public Label lbl_subtotal;
+    public Label lbl_total_libras;
+    public DatePicker date_fecha_proceso;
+    public Label lbl_id_remision;
+    public TextField txt_entradas_salidas;
+    public Label lbl_id_tabaco;
+    public Label lbl_id_pilon;
+    public TextField txt_subtotal;
+    public TextField txt_total_libras;
+    public Button btn_guardar_proceso_pilon;
+    public Button btn_actualizar_proceso_pilon;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         super.start(primaryStage);
