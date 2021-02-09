101d387a58badc844424d9296a3336b841cf2728
Nahun Flores <nahun.a.f.a90@gmail.com>
2021-02-09 07:46:32 -0600

    ventana resplandeciente

---
+++ src/clases/SidePanelController.java
@@ -0,0 +1,22 @@
+package clases;
+
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SidePanelController extends Aplicacion_principal implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- src/clases/pantalla_principal.java
+++ src/clases/pantalla_principal.java
@@ -1,14 +1,27 @@
 package clases;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
-public class pantalla_principal extends Aplicacion_principal {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class pantalla_principal extends Aplicacion_principal implements Initializable {
+
+
+    public StackPane stackpane;
+
 
 
     @Override
@@ -26,9 +39,55 @@ public class pantalla_principal extends Aplicacion_principal {
         stage.setWidth(1200);
         stage.setTitle("");
         stage.show();
+    }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
-
+        if (!Main.ventana_splash) {
+            loadSplashScreen();
+        }
 
     }
+    private void loadSplashScreen() {
+            {
+                try {
+
+                    Main.ventana_splash = true;
+
+                    StackPane pane = FXMLLoader.load(getClass().getResource(("/resources/pantalla.fxml")));
+                    stackpane.getChildren().setAll(pane);
+
+                    FadeTransition fadeIn = new FadeTransition(Duration.seconds(3), pane);
+                    fadeIn.setFromValue(0);
+                    fadeIn.setToValue(1);
+                    fadeIn.setCycleCount(1);
+
+                    FadeTransition fadeOut = new FadeTransition(Duration.seconds(3), pane);
+                    fadeOut.setFromValue(1);
+                    fadeOut.setToValue(0);
+                    fadeOut.setCycleCount(1);
+
+                    fadeIn.play();
+
+                    fadeIn.setOnFinished((e) -> {
+                        fadeOut.play();
+                    });
+
+                    fadeOut.setOnFinished((e) -> {
+                        try {
+                            StackPane parentContent = FXMLLoader.load(getClass().getResource(("/resources/pantalla_principal.fxml")));
+                            stackpane.getChildren().setAll(parentContent);
+                        } catch (IOException ex) {
+                            Logger.getLogger(pantalla_principal.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    });
+
+                } catch (IOException ex) {
+                    Logger.getLogger(pantalla_principal.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+        }
+
 }
