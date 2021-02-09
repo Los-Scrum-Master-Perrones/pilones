681e205aa84434580a289a2bc61726f50a1e6b93
LuisReyes504 <luismiguelreyes451@gmail.com>
2021-02-09 14:58:09 -0600

    variables de referencia por pantalla

--- src/clases/clase_tabaco.java
+++ src/clases/clase_tabaco.java
@@ -3,16 +3,26 @@ package clases;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class clase_tabaco extends Aplicacion_principal{
 
+    public Label lbl_nombre_tabaco;
+    public TextField txt_nombre_tabaco;
+    public Button btn_guardar_clase_tabaco;
+    public Button btn_actualizar_clase_tabaco;
+    public Label lbl_id_clase_tabaco;
+    public Label lbl_id_tabaco;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         super.start(primaryStage);
-        Parent root = FXMLLoader.load(getClass().getResource("/resources/clase_tabaco1.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/clase_tabaco.fxml"));
 
         Scene scene = new Scene(root);
         Stage stage = new Stage();
--- src/clases/control_temperatura.java
+++ src/clases/control_temperatura.java
@@ -3,15 +3,30 @@ package clases;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class control_temperatura extends Aplicacion_principal{
+    public Label lbl_temperatura;
+    public Label lbl_fecha_revision;
+    public Label lbl_mantenimiento;
+    public DatePicker date_fecha_revision;
+    public TextField txt_mantenimiento;
+    public TextField txt_temperatura;
+    public Label lbl_id_pilones;
+    public Label lbl_id_pilon;
+    public Button btn_guardar;
+    public Button btn_actualizar;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         super.start(primaryStage);
-        Parent root = FXMLLoader.load(getClass().getResource("/resources/control_temperatura1.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/control_temperatura.fxml"));
 
         Scene scene = new Scene(root);
         Stage stage = new Stage();
