b5addb2cc7e63ee810e28da55f275156f4b871cf
Nahun Flores <nahun.a.f.a90@gmail.com>
2021-02-10 14:56:14 -0600

    Clases primerasa funciones

--- src/clases/SidePanelController.java
+++ src/clases/SidePanelController.java
@@ -12,13 +12,41 @@ public class SidePanelController extends Aplicacion_principal implements Initial
     public JFXButton boton_clase_tabaco;
     public JFXButton boton_registro_pilones;
     public JFXButton boton_temperatura;
+    public JFXButton boton_entradas_salidas;
+    public JFXButton boton_grafico_temperatura;
+    private pantalla_principal ventana_nueva;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
-    public void exit(ActionEvent actionEvent) {
-        System.exit(0);
+    public void setActualizaMain(pantalla_principal actualizaMain){
+        this.ventana_nueva = actualizaMain;
+
     }
+
+    public void abrir_ventanas(ActionEvent actionEvent) {
+        switch (((JFXButton)actionEvent.getSource()).getText()){
+            case "Clase tabaco":
+
+                break;
+            case "Registro Pilones":
+
+                break;
+            case "Registro Temperatura":
+
+                break;
+            case "Entradas y Salidas":
+
+                break;
+            case "Gráfico de Temperatura":
+
+                break;
+        }
+
+    }
+
+
 }
