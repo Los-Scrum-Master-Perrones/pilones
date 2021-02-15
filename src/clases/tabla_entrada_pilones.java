eaa762bf5dd9ecfd6139910d4021ad0ef02617e4
KeniaColindress <colindreskenia13@gmail.com>
2021-02-15 16:09:58 -0600

    clase entrada pilones

---
+++ src/clases/tabla_entrada_pilones.java
@@ -0,0 +1,21 @@
+package clases;
+
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class tabla_entrada_pilones extends Aplicacion_principal implements Initializable {
+
+    public void cargar(Stage PrimaryStage) throws Exception{
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
