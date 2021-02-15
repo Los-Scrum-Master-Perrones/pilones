840cc271f05b4539f76857cf60a971f0af639b1a
LuisReyes504 <luismiguelreyes451@gmail.com>
2021-02-15 16:45:02 -0600

    cargar datos de la base a la tabla clase tabaco para pilon

--- src/clases/tabla_clase_tabaco.java
+++ src/clases/tabla_clase_tabaco.java
@@ -1,23 +1,33 @@
 package clases;
 
+import clases.DBUtilities.DBType;
+import clases.DBUtilities.DBUtilities;
 import clases.Objetos_POJO.Clase_tabacos;
 import com.jfoenix.controls.JFXTreeTableColumn;
 import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class tabla_clase_tabaco extends Aplicacion_principal implements Initializable {
 
-    public JFXTreeTableView jt_clase_tabaco_pilon;
+    public JFXTreeTableView<Clase_tabacos> jt_clase_tabaco_pilon;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -42,6 +52,7 @@ public class tabla_clase_tabaco extends Aplicacion_principal implements Initiali
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        // para crear la tabla
         JFXTreeTableColumn<Clase_tabacos,String> _1 = new JFXTreeTableColumn<>("ID");
         JFXTreeTableColumn<Clase_tabacos,String> _2 = new JFXTreeTableColumn<>("Clase Tabaco");
 
@@ -58,5 +69,35 @@ public class tabla_clase_tabaco extends Aplicacion_principal implements Initiali
         _2.setCellValueFactory(
                 new TreeItemPropertyValueFactory<Clase_tabacos,String>("nombre_tbc")
         );
+
+
+        //se inicializa el metodo Cargar
+        try {
+            Cargar_tabla_tabaco_pilon();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+     // Cargar los datos de la tabla
+    public void Cargar_tabla_tabaco_pilon() throws SQLException, ClassNotFoundException {
+        PreparedStatement consulta_tabaco = DBUtilities.getConnection(DBType.MARIADB).prepareStatement(
+                "SELECT * FROM clase_tabaco");
+        ResultSet resultSet_tabaco = consulta_tabaco.executeQuery();
+
+        ObservableList<Clase_tabacos> data_tabaco = FXCollections.observableArrayList();
+        while (resultSet_tabaco.next()){
+            data_tabaco.add(new Clase_tabacos(resultSet_tabaco.getString(1),
+                    resultSet_tabaco.getString(2)
+            ));
+        }
+        TreeItem<Clase_tabacos> root = new RecursiveTreeItem<>(data_tabaco, RecursiveTreeObject::getChildren);
+
+        jt_clase_tabaco_pilon.setRoot(root);
+        jt_clase_tabaco_pilon.setShowRoot(false);
+
+    }
+
 }
