de3747e8fe69c4619ccf42f0a8b4a468602ef849
LuisReyes504 <luismiguelreyes451@gmail.com>
2021-02-15 15:46:57 -0600

    otra vez base de datos

--- src/resources/bd_taopar_pilones_act.sql
+++ src/resources/bd_taopar_pilones_act.sql
@@ -11,19 +11,49 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- La exportación de datos fue deseleccionada.
 
--- La exportación de datos fue deseleccionada.
+-- Volcando estructura de base de datos para db_taopar_pilones
+CREATE DATABASE IF NOT EXISTS `db_taopar_pilones` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `db_taopar_pilones`;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla db_taopar_pilones.clase_tabaco: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `clase_tabaco` DISABLE KEYS */;
+REPLACE INTO `clase_tabaco` (`id_tabaco`, `nombre_tabaco`) VALUES
+	(1, 'Hola');
+/*!40000 ALTER TABLE `clase_tabaco` ENABLE KEYS */;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla db_taopar_pilones.control_temperatura: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `control_temperatura` DISABLE KEYS */;
+REPLACE INTO `control_temperatura` (`id_temperatura`, `id_pilones`, `temperatura`, `fecha_revision`, `mantenimiento`) VALUES
+	(2, 1, 80, '2021-02-15', 'Virado'),
+	(3, 1, 80, '2021-02-15', 'Mojado');
+/*!40000 ALTER TABLE `control_temperatura` ENABLE KEYS */;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla db_taopar_pilones.entrada_pilones: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `entrada_pilones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entrada_pilones` ENABLE KEYS */;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla db_taopar_pilones.pilones: ~5 rows (aproximadamente)
+/*!40000 ALTER TABLE `pilones` DISABLE KEYS */;
+REPLACE INTO `pilones` (`id_pilon`, `numero_pilon`) VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 4),
+	(5, 5);
+/*!40000 ALTER TABLE `pilones` ENABLE KEYS */;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla db_taopar_pilones.remision_proceso: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `remision_proceso` DISABLE KEYS */;
+/*!40000 ALTER TABLE `remision_proceso` ENABLE KEYS */;
+
+-- Volcando datos para la tabla db_taopar_pilones.tabla_pilon: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `tabla_pilon` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tabla_pilon` ENABLE KEYS */;
+
+-- Volcando datos para la tabla db_taopar_pilones.tabla_procesos: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `tabla_procesos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tabla_procesos` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
