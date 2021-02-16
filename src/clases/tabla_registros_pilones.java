9c01abb9a1e3e93c9faddced4c92f922c922f173
LuisReyes504 <luismiguelreyes451@gmail.com>
2021-02-16 10:14:39 -0600

    cargado de los datos de pilones a la tabla

--- src/clases/tabla_registros_pilones.java
+++ src/clases/tabla_registros_pilones.java
@@ -3,6 +3,7 @@ package clases;
 import clases.DBUtilities.DBType;
 import clases.DBUtilities.DBUtilities;
 import clases.Objetos_POJO.Clase_pilones;
+import clases.Objetos_POJO.Clase_pilones_nombre;
 import clases.Objetos_POJO.Clase_tabacos;
 import com.jfoenix.controls.JFXTreeTableColumn;
 import com.jfoenix.controls.JFXTreeTableView;
@@ -30,7 +31,7 @@ import java.util.ResourceBundle;
 
 public class tabla_registros_pilones extends Aplicacion_principal implements Initializable {
 
-    public JFXTreeTableView<Clase_pilones> jt_registro_pilones;
+    public JFXTreeTableView<Clase_pilones_nombre> jt_registro_pilones;
     public Button btn_guardar_registro_pilones;
     public Button btn_actualizar_registro_pilones;
     public TextField txt_buscar_registro_pilones;
@@ -53,8 +54,8 @@ public class tabla_registros_pilones extends Aplicacion_principal implements Ini
     public void initialize(URL location, ResourceBundle resources) {
 
         // para crear la tabla
-        JFXTreeTableColumn<Clase_pilones,String> _1 = new JFXTreeTableColumn<>("ID");
-        JFXTreeTableColumn<Clase_pilones,String> _2 = new JFXTreeTableColumn<>("Número de Pilón");
+        JFXTreeTableColumn<Clase_pilones_nombre,String> _1 = new JFXTreeTableColumn<>("ID");
+        JFXTreeTableColumn<Clase_pilones_nombre,String> _2 = new JFXTreeTableColumn<>("Número de Pilón");
 
 
         _1.setPrefWidth(50);
@@ -63,17 +64,17 @@ public class tabla_registros_pilones extends Aplicacion_principal implements Ini
         jt_registro_pilones.getColumns().addAll(_1,_2);
 
         _1.setCellValueFactory(
-                new TreeItemPropertyValueFactory<Clase_pilones,String>("id_tabaco")
+                new TreeItemPropertyValueFactory<Clase_pilones_nombre,String>("id_pilon")
         );
 
         _2.setCellValueFactory(
-                new TreeItemPropertyValueFactory<Clase_pilones,String>("nombre_tbc")
+                new TreeItemPropertyValueFactory<Clase_pilones_nombre,String>("nombre_pilon")
         );
 
 
         //se inicializa el metodo Cargar
         try {
-            Cargar_tabla_tabaco_pilon();
+            Cargar_tabla_pilon();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -82,18 +83,18 @@ public class tabla_registros_pilones extends Aplicacion_principal implements Ini
     }
 
     // Cargar los datos de la tabla
-    public void Cargar_tabla_tabaco_pilon() throws SQLException, ClassNotFoundException {
+    public void Cargar_tabla_pilon() throws SQLException, ClassNotFoundException {
         PreparedStatement consulta_tabaco = DBUtilities.getConnection(DBType.MARIADB).prepareStatement(
-                "SELECT * FROM clase_tabaco");
-        ResultSet resultSet_tabaco = consulta_tabaco.executeQuery();
+                "SELECT * FROM pilones");
+        ResultSet resultSet_pilones = consulta_tabaco.executeQuery();
 
-        ObservableList<Clase_pilones> data_tabaco = FXCollections.observableArrayList();
-        while (resultSet_tabaco.next()){
-            data_tabaco.add(new Clase_pilones(resultSet_tabaco.getString(1),
-                    resultSet_tabaco.getString(2)
+        ObservableList<Clase_pilones_nombre> data_pilones = FXCollections.observableArrayList();
+        while (resultSet_pilones.next()){
+            data_pilones.add(new Clase_pilones_nombre(resultSet_pilones.getString(1),
+                    resultSet_pilones.getString(2)
             ));
         }
-        TreeItem<Clase_pilones> root = new RecursiveTreeItem<>(data_tabaco, RecursiveTreeObject::getChildren);
+        TreeItem<Clase_pilones_nombre> root = new RecursiveTreeItem<>(data_pilones, RecursiveTreeObject::getChildren);
 
         jt_registro_pilones.setRoot(root);
         jt_registro_pilones.setShowRoot(false);
