f09c62f42e7f8621fe86836b33214ea974db8a75
Nahun Flores <nahun.a.f.a90@gmail.com>
2021-02-22 08:39:50 -0600

    MAs modificaciones

--- src/clases/Aplicacion_principal.java
+++ src/clases/Aplicacion_principal.java
@@ -3,8 +3,11 @@ package clases;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.JFXTextField;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -30,5 +33,49 @@ public class Aplicacion_principal extends Application {
 
         ventana_mensaje.setActions(btn_mensaje);
         dialogo.show();
+    }
+
+
+    public void soloNumerosyunPunto(final JFXTextField campo, int limite, int conPunto) {
+
+        campo.setOnKeyTyped(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(javafx.scene.input.KeyEvent event) {
+                if (campo.getText().length() == 0 && event.getCharacter().equals(" ")) {
+                    event.consume();
+                }
+                if (campo.getText().length() == 0 && event.getCharacter().equals(".")) {
+                    event.consume();
+                }
+                if (campo.getText().length() <= limite) {
+                    if (!Character.isDigit(event.getCharacter().charAt(0)) && !(event.getCharacter().equals("."))) {
+                        event.consume();
+                    }
+                    if (event.getCharacter().equals(".") && campo.getText().contains(".")) {
+                        event.consume();
+                    }
+
+                } else {
+
+                    if (event.getCharacter().equals(".") || campo.getText().contains(".")) {
+
+                        if (campo.getText().length() <= conPunto) {
+                            if ((!Character.isDigit(event.getCharacter().charAt(0)) && !(event.getCharacter().equals(".")))
+                                    || (event.getCharacter().equals(".") && campo.getText().contains("."))) {
+                                event.consume();
+                            } else {
+
+                            }
+                        }else{
+                            event.consume();
+                        }
+
+
+                    } else {
+                        event.consume();
+                    }
+                }
+            }
+        });
     }
 }
