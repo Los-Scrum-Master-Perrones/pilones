0b73830d156bba902ae6f96ecc9e27f6585f0ae7
Nahun Flores <nahun.a.f.a90@gmail.com>
2021-02-24 15:11:44 -0600

    mas cambios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
