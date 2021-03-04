4fc0471834fd057b6267cd92e3bbaebf459551ee
KeniaColindress <colindreskenia13@gmail.com>
2021-03-04 11:40:35 -0600

    ajustes

---
+++ src/main/java/control_pilones.java
@@ -0,0 +1,25 @@
+import Objetos_POJO.Clase_pilones_nombre;
+import Objetos_POJO.Clase_tabacos;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class control_pilones extends Aplicacion_principal implements Initializable {
+
+
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
