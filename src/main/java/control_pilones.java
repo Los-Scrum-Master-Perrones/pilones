857b636b943b17dde87b5142eeccc32c3d562f08
KeniaColindress <colindreskenia13@gmail.com>
2021-03-09 09:09:09 -0600

    Ajustes

--- src/main/java/control_pilones.java
+++ src/main/java/control_pilones.java
@@ -3,9 +3,15 @@ import Objetos_POJO.Clase_tabacos;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,6 +26,24 @@ public class control_pilones extends Aplicacion_principal implements Initializab
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        super.start(primaryStage);
+        Parent root = FXMLLoader.load(getClass().getResource("/tabla_control_pilones.fxml"));
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setResizable(false);
+        stage.setTitle("Control pilones");
+        stage.show();
+
+
 
     }
 }
